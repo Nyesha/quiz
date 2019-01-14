88027789654029dde4f45482d77d47801300c4d3
Nyesha Jones <neeshajolmfao@gmail.com>
2019-01-14 23:11:31 +0000

    Love Scenario- iKON p.s Final Touches

--- js/script.js
+++ js/script.js
@@ -17,14 +17,31 @@ $(document).ready(function() {
          } else if (q1Result ==="Studying") {
         totalScore= totalScore + 10;
          }
-         if (q2Result === ""){
+         if (q2Result === "Fiery"){
              totalScore= totalScore+5;
-         } else if (q2Result === "") {
-             
-         }
-         }
+         } else if (q2Result === "Nonchalant") {
+             totalScore= totalScore +7;
+             } else if (q2Result === "Helpful") {
+                 totalScore = totalScore === +10;
+             }
+             if (q3Result === "Coffee"){
+                 totalScore=totalScore+5;
+             } else if (q3Result ==="Tea"){
+                 totalScore = totalScore +7 
+                 } else if (q3Result === "Water"){
+                     totalScore = totalScore +10;
+                 }
         if(totalScore < 30)
         $(".result").text("Shoto Todoroki")
+         if(totalScore > 10 )
+         $(".result").text("Eijiro Kirishima")
+         if(totalScore === 21)
+         $(".result").text("Shoto Todoroki")
+         if(totalScore === 15)
+         $(".result").text("Izuku Midoriya")
+         if(totalScore ===0)
+         $(".result").text("Play The Game To Get A Character")
+         
     
     });
     
